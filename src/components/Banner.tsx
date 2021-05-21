1a7b8a3243553afeb2ace0e00838b0589c0f0727
Gean Lucas <gean.lfpsa@gmail.com>
2021-05-20 23:03:44 -0300

    Banner component structure created

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,31 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, VStack, Img, Text } from "@chakra-ui/react";
 
 export function Banner() {
   return (
     <Flex
-      h="8rem"
+      h="10rem"
       w="100%"
       bg="blue.500"
+      align="center"
     >
-      <Text>
-        5 Continentes,
-        infinitas possibilidades.
-      </Text>
+      <Flex
+        px="5rem"
+        justify="space-between"
+        align="center"
+        w="100%"
+      >
+        <VStack spacing="4">
+          <Text>
+            5 Continentes,
+            infinitas possibilidades.
+        </Text>
+          <Text>
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+        </Text>
+        </VStack>
+
+        <Img src="/airplane.svg" h="6rem" />
+      </Flex>
     </Flex>
   );
 }
